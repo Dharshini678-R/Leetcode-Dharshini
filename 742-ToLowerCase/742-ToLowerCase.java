5ffd260b86b6e043717f29921c62e64813654da5
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:09:12 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,14 @@
+// Last updated: 8/11/2026, 4:09:12 PM
+class Solution {
+    public String toLowerCase(String s) {
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                arr[i] = (char)(arr[i] + 32);
+            }
+        }
+
+        return new String(arr);
+    }
+}
