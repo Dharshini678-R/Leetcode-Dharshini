b7460ba687d0b717c624c947a04aca2340de1066
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:06:06 +0530

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.java
@@ -0,0 +1,17 @@
+// Last updated: 8/11/2026, 4:06:05 PM
+class Solution {
+    public int removePalindromeSub(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return 2;
+            }
+            left++;
+            right--;
+        }
+
+        return s.isEmpty() ? 0 : 1;
+    }
+}
