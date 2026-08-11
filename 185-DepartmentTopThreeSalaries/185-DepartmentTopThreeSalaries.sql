7368caac94f3be3fc5b2c8f6f32460fffb6570fa
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:14:40 +0530

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/11/2026, 4:14:40 PM
+# Write your MySQL query statement below
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+    AND salary > e.salary
+) < 3;
