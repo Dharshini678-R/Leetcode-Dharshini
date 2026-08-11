aaa6d30ebef93dc9038de82aa3c163ec170955ba
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:07:00 +0530

    Create 1228-MinimumCostTreeFromLeafValues

---
+++ 1228-MinimumCostTreeFromLeafValues/1228-MinimumCostTreeFromLeafValues.java
@@ -0,0 +1,19 @@
+// Last updated: 8/11/2026, 4:06:59 PM
+class Solution {
+    public int mctFromLeafValues(int[] arr) {
+        int res = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Integer.MAX_VALUE);
+        for (int num : arr) {
+            while (stack.peek() <= num) {
+                int mid = stack.pop();
+                res += mid * Math.min(stack.peek(), num);
+            }
+            stack.push(num);
+        }
+        while (stack.size() > 2) {
+            res += stack.pop() * stack.peek();
+        }
+        return res;
+    }
+}
