a154ea7115d462d9a4a4fb108ebf0dfc94b48d10
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:09:36 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,26 @@
+// Last updated: 8/11/2026, 4:09:35 PM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+
+        for (char ch : moves.toCharArray()) {
+            switch (ch) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
