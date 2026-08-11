1ef2453ba89cb3b4ab8d2d2a5423a89016d04b33
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:13:04 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 4:13:03 PM
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
