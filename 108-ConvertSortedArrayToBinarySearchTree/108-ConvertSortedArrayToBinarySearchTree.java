05483da1e50c2597910916a018eaf447e89eb668
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:16:07 +0530

    Create 108-ConvertSortedArrayToBinarySearchTree

---
+++ 108-ConvertSortedArrayToBinarySearchTree/108-ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 4:16:07 PM
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int l, int r) {
+        if (l > r) return null;
+        int m = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = build(nums, l, m - 1);
+        root.right = build(nums, m + 1, r);
+        return root;
+    }
+}
