bb1f34dce143b5b451b0e5e98af5def57e31d964
Dharshini678-R <shinid678@gmail.com>
2026-08-11 16:06:46 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/11/2026, 4:06:46 PM
+
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+                        AND '2019-07-27'
+GROUP BY activity_date;
