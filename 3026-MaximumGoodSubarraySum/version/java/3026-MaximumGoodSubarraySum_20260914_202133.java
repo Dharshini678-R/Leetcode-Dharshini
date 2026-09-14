e9f8a5cbbe108b9800efc7fbee5b0713e3abebf0
Dharshini678-R <shinid678@gmail.com>
2026-09-14 20:21:35 +0530

    Create 3026-MaximumGoodSubarraySum

---
+++ 3026-MaximumGoodSubarraySum/version/java/3026-MaximumGoodSubarraySum_20260914_202133.java
@@ -0,0 +1,22 @@
+// Last updated: 9/14/2026, 8:21:33 PM
+1import java.util.*;
+2
+3class Solution {
+4    public List<List<String>> groupAnagrams(String[] strs) {
+5
+6        HashMap<String, List<String>> map = new HashMap<>();
+7
+8        for (String s : strs) {
+9
+10            char[] chars = s.toCharArray();
+11            Arrays.sort(chars);
+12
+13            String key = new String(chars);
+14
+15            map.putIfAbsent(key, new ArrayList<>());
+16            map.get(key).add(s);
+17        }
+18
+19        return new ArrayList<>(map.values());
+20    }
+21}
